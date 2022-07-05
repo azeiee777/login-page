de4bf82bc2d68b6a91b09477a2491ee06ccac4e1
Abdul <Abdul002ajij@gmail.com>
2022-07-05 15:04:45 +0530

    project creation initialised

---
+++ app/src/main/java/com/aziztraders/ui/login/view/LoginPage.java
@@ -0,0 +1,17 @@
+package com.aziztraders.ui.login.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.aziztraders.loginpage.R;
+
+public class LoginPage extends AppCompatActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+}
